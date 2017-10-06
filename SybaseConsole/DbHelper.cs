ca0ad3fc1a8b700cf5c71e8209d776f87a98220c
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2017-10-06 20:23:04 +0530

    Initial Checkin

---
+++ SybaseConsole/DbHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sybase.Data.AseClient;
+using System.Data;
+using System.Configuration;
+
+namespace SybaseConsole
+{
+    public class DbHelper
+    {
+        private AseConnection _dbConn;
+        public DbHelper() { }
+        private string GetConnectionString()
+        {
+            if (ConfigurationManager.ConnectionStrings["sybase-connectionstring"] != null)
+                return ConfigurationManager.ConnectionStrings["sybase-connectionstring"].ConnectionString;
+            else
+                return "";
+        }
+        private void OpenConnection()
+        {
+            if (_dbConn != null)
+            {
+                if (_dbConn.State == ConnectionState.Closed)
+                {
+                    _dbConn.Open();
+                }
+            }
+        }
+        public IDataReader ExecuteReader(string query)
+        {
+            _dbConn = new AseConnection(GetConnectionString());
+            AseCommand dbCommand = new AseCommand(query, _dbConn);
+            dbCommand.CommandType = CommandType.Text;
+            OpenConnection();
+            return dbCommand.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+    }
+}
---
+++ SybaseConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SybaseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbHelper helper = new DbHelper();
+            var dataReader =  helper.ExecuteReader("SELECT syscolumns.name, systypes.name FROM sysobjects JOIN syscolumns ON sysobjects.id = syscolumns.id JOIN systypes ON systypes.type = syscolumns.type AND systypes.usertype = syscolumns.usertype WHERE sysobjects.name LIKE 'Claims'");
+
+            while (dataReader.Read())
+            {
+                Console.WriteLine(dataReader[0] + "," + dataReader[1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
